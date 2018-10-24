685ef375f7b34d1726c7d5a677787791d1e60366
nerfen <kolyak2000@yandex.by>
2018-10-24 19:15:01 +0300

    add sss

--- Assets/Scripts/ControllerButton.cs
+++ Assets/Scripts/ControllerButton.cs
@@ -11,12 +11,15 @@ public class ControllerButton : MonoBehaviour {
     public Dropdown rasa;
     public Dropdown pol;
 
+    public GameObject buttleUI;
+    public GameObject menuUI;
+
     public Player player;
 	
 	void Start () {
         // rasa = GetComponent<Dropdown>();
         start.onClick.AddListener(() => start_game(rasa.value,pol.value,name.text));
-        
+      
     }
 	
 	void start_game(int rasa,int pol, string name)
@@ -24,5 +27,12 @@ public class ControllerButton : MonoBehaviour {
         player.Selected_rasa = rasa;
         player.Name = name;
         player.Man_woman = pol;
+
+        menuUI.SetActive(true);
+        buttleUI.SetActive(true);
+
+
+
+
     }
 }
