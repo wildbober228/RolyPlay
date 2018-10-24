9e373c14d53854dbadbb55f3a38f4eeb5e2a9f1f
wildbober228 <irina20000@tut.by>
2018-10-24 15:25:38 +0300

    1.0

---
+++ Assets/Scripts/MagicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicPlayer : Player
+{
+
+   
+	void Start () {
+        ID = 53;
+
+        Debug.Log(ID);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+     int id_player = 0;
+    string name_player;
+    enum status
+    {
+        norm,
+        weak,
+        ill,
+        poisoned,
+        paralis,
+        dead
+    }
+    bool can_speak;
+    bool can_go;    
+    enum rasa
+    {
+        human,
+        gnom,
+        elf,
+        ork,
+        goblin
+    }
+    int man_woman;
+    int age;
+    float health;
+    float max_health;
+    int xp;
+
+    public int ID
+    {
+        get { return id_player; }
+
+        set { id_player = value; }
+
+    }
+
+    private void Start()
+    {
+        set_id();
+        name_player = "Edic";
+    }
+
+    private void set_id()
+    {
+        ID = id_player;
+        id_player++;
+    }
+}
